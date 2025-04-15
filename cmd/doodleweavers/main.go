227cbdade6217882c0010bd0c87c02ff20c10010
vrnraju <vrnraju@gmail.com>
2025-04-15 12:28:28 +0530

    added basic webscoket connection

--- cmd/doodleweavers/main.go
+++ cmd/doodleweavers/main.go
@@ -3,10 +3,46 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"github.com/gorilla/websocket"
 )
 
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+func handleConnections(w http.ResponseWriter, r *http.Request) {
+	log.Println("Received connection attempt...")
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Printf("Failed to upgrade Connc")
+		return
+	}
+	defer ws.Close()
+	log.Println("Client Connected!!")
+
+	for {
+		messageType, message, err := ws.ReadMessage()
+		if err != nil {
+			log.Println("Read error:", err)
+			break
+		}
+		err = ws.WriteMessage(messageType, message)
+		if err != nil {
+			log.Println("Write error:", err)
+			break
+		}
+	}
+	log.Println("Websocket connection closed. ")
+
+}
 func main() {
 	//first
+	http.HandleFunc("/ws", handleConnections)
 	serverAddress := ":8080"
 	log.Printf("Starting Doodle Weavers server on %s\n", serverAddress)
 
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/vrnraju/doodle-weavers
 
 go 1.22.3
+
+require github.com/gorilla/websocket v1.5.3 // indirect
